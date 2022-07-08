7e9ba303b532209b69356a62e49ac3769a82ca8e
Paas-an <jolsenskogtro@gmail.com>
2022-07-08 09:08:12 +0200

    basic layout

---
+++ Services/ClippboardService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+namespace webapp.Services
+{
+    public class ClippboardService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ClippboardService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
+

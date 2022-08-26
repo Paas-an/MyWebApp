18b68e50b706aaeebfee96c5998e3966f88ee63b
Paas-an <jolsenskogtro@gmail.com>
2022-08-26 05:25:31 +0200

    asdfasdf

--- Program.cs
+++ Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<webapp.Services.ClippboardService>();
 await builder.Build().RunAsync();
+builder.Services.AddScoped<webapp.Services.IClipboardService, webapp.Services.ClipboardService>();
+
